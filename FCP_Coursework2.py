1f85904160f99c7cc44f539a07a3d03052738e29
Louie-B <124567974+Louie-B@users.noreply.github.com>
2023-03-20 10:14:10 +0000

    Add files via upload
    recursive solver

---
+++ FCP_Coursework2.py
@@ -0,0 +1,183 @@
+import copy
+import time
+import random
+import time
+#Grids 1-5 are 2x2
+grid1 = [
+		[1, 0, 4, 2],
+		[4, 2, 1, 3],
+		[2, 1, 3, 4],
+		[3, 4, 2, 1]]
+
+grid2 = [
+		[1, 0, 4, 2],
+		[4, 2, 1, 3],
+		[2, 1, 0, 4],
+		[3, 4, 2, 1]]
+
+grid3 = [
+		[1, 0, 4, 2],
+		[4, 2, 1, 0],
+		[2, 1, 0, 4],
+		[0, 4, 2, 1]]
+
+grid4 = [
+		[1, 0, 4, 2],
+		[0, 2, 1, 0],
+		[2, 1, 0, 4],
+		[0, 4, 2, 1]]
+
+grid5 = [
+		[1, 0, 0, 2],
+		[0, 0, 1, 0],
+		[0, 1, 0, 4],
+		[0, 0, 0, 1]]
+
+grid6 = [
+		[0, 0, 6, 0, 0, 3],
+		[5, 0, 0, 0, 0, 0],
+		[0, 1, 3, 4, 0, 0],
+		[0, 0, 0, 0, 0, 6],
+		[0, 0, 1, 0, 0, 0],
+		[0, 5, 0, 0, 6, 4]]
+
+grids = [(grid1, 2, 2), (grid2, 2, 2), (grid3, 2, 2), (grid4, 2, 2), (grid5, 2, 2)]
+'''
+===================================
+DO NOT CHANGE CODE ABOVE THIS LINE
+===================================
+'''
+def check_section(section, n):
+
+	if len(set(section)) == len(section) and sum(section) == sum([i for i in range(n+1)]):
+		return True
+	return False
+
+
+def get_squares(grid, n_rows, n_cols):
+
+	squares = []
+	for i in range(n_cols):
+		rows = (i*n_rows, (i+1)*n_rows)
+		for j in range(n_rows):
+			cols = (j*n_cols, (j+1)*n_cols)
+			square = []
+			for k in range(rows[0], rows[1]):
+				line = grid[k][cols[0]:cols[1]]
+				square +=line
+			squares.append(square)
+
+
+	return(squares)
+
+
+def check_solution(grid, n_rows, n_cols):
+	'''
+	This function is used to check whether a sudoku board has been correctly solved
+
+	args: grid - representation of a suduko board as a nested list.
+	returns: True (correct solution) or False (incorrect solution)
+	'''
+	n = n_rows*n_cols
+
+	for row in grid:
+		if check_section(row, n) == False:
+			return False
+
+	for i in range(n_rows):
+		column = []
+		for row in grid:
+			column.append(row[i])
+		if check_section(column, n) == False:
+			return False
+
+	squares = get_squares(grid, n_rows, n_cols)
+	for square in squares:
+		if check_section(square, n) == False:
+			return False
+
+	return True
+ 
+
+def recursive_solve(grid, n_rows, n_cols):
+#N is the maximum integer considered in this board
+	reset = grid
+	guesses = []
+	N = n_rows*n_cols
+	list_column = []
+	list_column_grid = []
+
+#Creating list of columns
+	for i in range(N):
+		for c in range(N):
+			list_column.append(grid[c][i])
+		list_column_grid.append(list_column)
+		list_column = []
+	zeros = False            #Used to check if any 0 are present within the grid
+
+
+	for row in range(N):   #Checking each row
+		for cols in range(len(grid[i])):
+			if grid[row][cols] == 0:       #Checks where zeros are present
+				zeros = True                        #True when there are still zeros present in grid
+				for num in range(1,N+1): 
+					if grid[row].count(num) == 0 and list_column_grid[cols].count(num) == 0:    #Checking if potential number is not present within row and column
+							guesses.append(num)
+				for guess in guesses:                 #Guesses is a list of potential numbers that could be a solutiion
+					grid[row][cols] = guess
+					if recursive_solve(grid,n_rows, n_cols): 
+						return grid
+				guesses = [] # Resets the guesses
+
+# Checking if the solution is correct. Grid only returned when there are no zeros present
+	if zeros == False and check_solution(grid, n_rows, n_cols) == True:
+		return grid
+
+
+def random_solve(grid, n_rows, n_cols, max_tries=5):
+
+		return grid
+
+
+def solve(grid, n_rows, n_cols):
+
+	'''
+	Solve function for Sudoku coursework.
+	Comment out one of the lines below to either use the random or recursive solver
+	'''
+
+	#return random_solve(grid, n_rows, n_cols)
+	return recursive_solve(grid, n_rows, n_cols)
+
+
+'''
+===================================
+DO NOT CHANGE CODE BELOW THIS LINE
+===================================
+'''
+def main():
+
+	points = 0
+
+	print("Running test script for coursework 1")
+	print("====================================")
+	
+	for (i, (grid, n_rows, n_cols)) in enumerate(grids):
+		print("Solving grid: %d" % (i+1))
+		start_time = time.time()
+		solution = solve(grid, n_rows, n_cols)
+		elapsed_time = time.time() - start_time
+		print("Solved in: %f seconds" % elapsed_time)
+		print(solution)
+		if check_solution(solution, n_rows, n_cols):
+			print("grid %d correct" % (i+1))
+			points = points + 10
+		else:
+			print("grid %d incorrect" % (i+1))
+
+	print("====================================")
+	print("Test script complete, Total points: %d" % points)
+
+
+if __name__ == "__main__":
+	main()
